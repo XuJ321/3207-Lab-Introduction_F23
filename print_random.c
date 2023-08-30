0dae813a79a3290e65424d653aa5a06151f5ebbf
XJ114514 <j10047392@gmail.com>
2023-08-30 09:40:07 -0400

    commit 1

--- print_random.c
+++ print_random.c
@@ -1,19 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
+
 #include <time.h>
+#include "header.h"
+
 
 
 
 int main()
 {
-	int a;
+	int a,temp;
 
 	/* seed the randomizer */
 	srand( (unsigned)time(NULL) );
+	
 
 	printf("Today's random word: ");
-	for(a=0;a<7;a++)
-		putchar( randchar() );
+	for(a=0;a<7;a++){
+		temp = rand()%27;
+		putchar( randchar(temp) );
+	}
 	putchar('\n');
 
 	return(0);
